fa4b65f0b3c58e3e69aa12798a01d77c126e8776
sapeint/chanku <sapeint@hashbang.sh>
2020-08-15 21:07:13 -0500

    Make pc, sp, regI are 16 Bit, Implement 0,1, and A
    The Registers have been made 16 bit where they are supposed to be.
    Additionally the Opcodes of 0, 1, and A have been implemented
    (somewhat).

--- src/chip8/chip8.c
+++ src/chip8/chip8.c
@@ -6,12 +6,11 @@
 #include "chip8.h"
 #include "../config.h"
 
-static uint8_t delay, sound, pc, sp, regI;
+static uint8_t delay, sound;
+static uint16_t pc, sp, regI;
 static uint8_t reg[16] = {0};
 static uint8_t mem[CHIP8_RAM_SIZE] = {0};
-
-typedef opdecptr void (*)(void);
-static opdecptr *jmptbl[];
+_Bool display[CHIP8_DISPLAY_HEIGHT][CHIP8_DISPLAY_WIDTH] = {false};
 
 void
 Chip8_Init(void){
@@ -33,15 +32,55 @@ Chip8_LoadROM(uint8_t rom[], size_t rom_size){
 
 _Bool
 Chip8_Cycle(void){
+    pc += 2;
+    Chip8_OpExec();
     return true;
 }
 
+
 _Bool
-Chip8_Decode(void){
+Chip8_OpExec(void){
     return true;
-}
+};
 
 _Bool
 Chip8_Shutdown(void){
     return true;
+}
+
+void
+Decode_0(void){
+    switch(mem[pc+1]){
+        case 0x0E:
+            /* Clear the Display */
+            0:
+            break;
+        case 0xEE:
+            break;
+        default:
+            /* No Op */
+            break
+    }
+}
+
+inline void
+Decode_1(void){
+    pc = (mem[pc] && 0x0F) * 256; /*(mem[pc] && 0x0F) << 4*/
+    pc += mem[pc+1];
+}
+
+inline void
+Decode_6(void){
+    reg[mem[pc] && 0x0F] = mem[pc+1];
+}
+
+inline void
+Decode_A(void){
+    regI = (mem[pc] && 0x0F) * 256; /*(mem[pc] && 0x0F) << 4*/
+    regI += (mem[pc+1]);
+}
+
+inline void
+Decode_D(void){
+    ; /* Currently a no-op, put the actual code here */
 }
