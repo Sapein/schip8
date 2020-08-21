4b8c696622e4ea4706823c22569d91db017a7768
sapeint/chanku <sapeint@hashbang.sh>
2020-08-20 23:15:47 -0500

    Actually work on emulating things, fix loading.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include "config.h"
 #include "chip8/chip8.h"
 
@@ -13,20 +14,18 @@ romload(char *path);
 int
 main(int argc, char *argv[]){
     int success = 0;
-
     Chip8_Init();
     if(argc > 0){
-        if(argparse(argc, argv)) success = 1, goto end;
-        if(romload(argv[argc - 1])) success = 1, goto end;
-
+        if(argparse(argc, argv)){success = 1; goto end;}
+        if(romload(argv[argc - 1])){success = 1; goto end;}
+        while(Chip8_Cycle());
     }else{
         fprintf(stderr, "You must provide a ROM file\n!");
     }
 
 end:
     Chip8_Shutdown();
-    return success;
-}
+    return success; }
 
 _Bool 
 argparse(int argc, char *argv[]){
@@ -41,32 +40,34 @@ argparse(int argc, char *argv[]){
             return false;
         }
     }
+    return false;
 }
 
 _Bool
 romload(char *path){
     FILE *romfile = NULL;
-    char rombuffer[CHIP8_RAM_SIZE] = {0};
+    unsigned char rombuffer[CHIP8_RAM_SIZE] = {0};
     long int romsize = 0;
     _Bool exit = false;
 
-    if((romfile = fopen(path)) != NULL){
+    if((romfile = fopen(path, "r")) != NULL){
         if(fseek(romfile, 0, SEEK_END) == 0){
-            if(romsize = ftell(romfile) == 0L){
+            if((romsize = ftell(romfile)) != -1L){
                 rewind(romfile);
                 if(fread(rombuffer, 1, romsize, romfile) == romsize){
-                    Chip8_LoadROM(rombuffer, (size_t) romsize)
+                    Chip8_LoadROM(rombuffer, (size_t) romsize);
                 }else{
                     perror("schip8 - fread:");
                     exit = true;
                 }
-            }else if(romsize -1L){
+            }else if(romsize == -1L){
                 perror("schip8 - ftell:");
                 exit = true;
             }
         }else{
             perror("schip8 - fseek:");
             exit = true;
+        }
     }
     fclose(romfile);
     romfile = NULL;
