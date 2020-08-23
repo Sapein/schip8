8aaed1921fe92562d5e90f0d6c0fa298c44e998f
sapeint/chanku <sapeint@hashbang.sh>
2020-08-22 22:04:48 -0500

    Fix numerous issues with various opcodes.
    Displaying now works properly. In addition OpCode 7 is now implemented,
    and OpCode A is properly implemented.
    
    Also OpCode 0x0E0 is now actually implemented.

--- src/chip8/chip8.c
+++ src/chip8/chip8.c
@@ -15,16 +15,16 @@ static uint8_t delay, sound;
 static uint16_t pc, sp, regI;
 static uint8_t reg[16] = {0};
 static uint8_t mem[CHIP8_RAM_SIZE] = {0};
- 
+
 _Bool Chip8_OpExec(void);
 
 static inline void
 updatedisp(uint8_t row, uint8_t col, _Bool bit) {
-        if(display[col][row] == true &&  bit == true){
-            display[col][row] = false;
+        if(display[row][col] == true && bit == true){
+            display[row][col] = false;
             reg[15] = 1;
-        }else{
-            display[col][row] = bit;
+        }else if(display[row][col] == false && bit == true){
+            display[row][col] = bit;
         }
 }
 
@@ -50,6 +50,7 @@ Chip8_LoadROM(uint8_t rom[], size_t rom_size){
 _Bool
 Chip8_Cycle(void){
     disp_chng = false;
+    pc+=2;
     Chip8_OpExec();
     if(pc > CHIP8_RAM_SIZE){
         return false;
@@ -66,16 +67,17 @@ Chip8_Shutdown(void){
 static void
 Decode_0(void){
     size_t row = 0;
-    size_t col = 9;
-    switch(mem[pc+1]){
-        case 0x0E:
+    size_t col = 0;
+    switch(mem[pc-1]){
+        case 0xE0:
             /* Clear the Display */
-            for(row = 0; row >= CHIP8_DISPLAY_HEIGHT; row++){
-                for(col = 0; col >= CHIP8_DISPLAY_WIDTH; col++){
+            for(row = 0; row < CHIP8_DISPLAY_HEIGHT; row++){
+                for(col = 0; col < CHIP8_DISPLAY_WIDTH; col++){
                     display[row][col] = false;
                 }
             }
             disp_chng = true;
+            break;
         case 0xEE:
             break;
         default:
@@ -86,17 +88,22 @@ Decode_0(void){
 
 inline static void
 Decode_1(void){
-    pc += ((mem[pc] - 0x10) * 256) + mem[pc]+1; /* pc += ((mem[pc] & 0x0F) << 8) | b */
+    pc += ((mem[pc - 2] - 0x10) * 256) + mem[pc-1]; /* pc += ((mem[pc-2] & 0x0F) << 8) | b */
 }
 
 inline static void
 Decode_6(void){
-    reg[mem[pc] - 0x60] = mem[pc+1]; /* reg[mem[pc] & 0x0F] = mem[pc+1] */
+    reg[mem[pc - 2] - 0x60] = mem[pc-1]; /* reg[mem[pc-2] & 0x0F] = mem[pc-1] */
 }
 
 inline static void
 Decode_A(void){
-    regI += ((mem[pc] - 0x10) * 256) + mem[pc]+1; /* regI += ((mem[pc] & 0x0F) << 8) | b */
+    regI = ((mem[pc - 2] - 0xA0) * 256) + mem[pc-1]; /* regI += ((mem[pc-2] & 0x0F) << 8) | b */
+}
+
+inline static void
+Decode_7(void){
+    reg[mem[pc-2] - 0x70] += mem[pc-1];
 }
 
 inline static void
@@ -104,59 +111,67 @@ Decode_D(void){
     /*TODO: Support wraparound Semantics */
     /*TODO: Actually detect edge of screen */
     /*TODO: Actually use bitwise operations */
-    uint8_t x = reg[mem[pc] - 0xD0]; /* reg[mem[pc] && 0x0F] */
-    uint8_t y = reg[mem[pc+1] / 16]; /* reg[mem[pc+1] >> 4] */
-    uint8_t sprite_height = mem[pc+1] - (16 * (mem[pc+1] / 16)); /* mem[pc + 1] & 0xF0 */
+    uint8_t x = reg[mem[pc - 2] - 0xD0]; /* reg[mem[pc-2] && 0x0F] */
+    uint8_t y = reg[mem[pc - 1] / 16]; /* reg[mem[pc-1] >> 4] */
+    uint8_t sprite_height = mem[pc-1] - (16 * (mem[pc-1] / 16)); /* mem[pc - 1] & 0xF0 */
     uint8_t row = 0;
+    uint8_t byte = 0;
     uint8_t bit = 0;
 
     reg[15] = 0;
-    for(row = 0; row == sprite_height; row++){
-        bit = ((((regI / 16) / 4) / 2));
+    for(row = 0; row < sprite_height; row++, regI++){
+        byte = mem[regI];
+        bit = ((((byte / 16) / 4) / 2));
         updatedisp(y + row, x+0, bit);
-        bit = ((((regI / 16) / 4) / 2)) - (2 * ((((regI / 16) / 4) / 2)));
+
+        bit = ((((byte / 16) / 4) / 2)) - (2 * ((((byte / 16) / 4) / 2)));
         updatedisp(y + row, x+1, bit);
-        bit = ((y / 16) - (4 * ((y / 16) / 4))) / 2;
+
+        bit = ((byte / 16) - (4 * ((byte / 16) / 4))) / 2;
         updatedisp(y + row, x+2, bit);
-        bit = ((y / 16) - (4 * ((y / 16) / 4))) - (2 * (((y / 16) - (4 * ((y / 16) / 4))) / 2));
+
+        bit = ((byte / 16) - (4 * ((byte / 16) / 4))) - (2 * (((byte / 16) - (4 * ((byte / 16) / 4))) / 2));
         updatedisp(y + row, x+3, bit);
-        bit = ((y - (16 * (y / 16))) / 4) / 2;
+
+        bit = ((byte - (16 * (byte / 16))) / 4) / 2;
         updatedisp(y + row, x+4, bit);
-        bit = ((y - (16 * (y / 16))) / 4) - (2 * (((y - (16 * (y / 16))) / 4) / 2));
+
+        bit = ((byte - (16 * (byte / 16))) / 4) - (2 * (((byte - (16 * (byte / 16))) / 4) / 2));
         updatedisp(y + row, x+5, bit);
-        bit = ((y - (16 * (y / 16))) - (4 * ((y - (16 * (y / 16))) / 4))) / 2;
+
+        bit = ((byte - (16 * (byte / 16))) - (4 * ((byte - (16 * (byte / 16))) / 4))) / 2;
         updatedisp(y + row, x+6, bit);
-        bit = ((y - (16 * (y / 16))) - (4 * ((y - (16 * (y / 16))) / 4))) - (2 * ((((y - (16 * (y / 16))) - (4 * ((y - (16 * (y / 16))) / 4))) / 2)));
+
+        bit = ((byte - (16 * (byte / 16))) - (4 * ((byte - (16 * (byte / 16))) / 4))) - (2 * ((((byte - (16 * (byte / 16))) - (4 * ((byte - (16 * (byte / 16))) / 4))) / 2)));
         updatedisp(y + row, x+7, bit);
     }
+    regI -= sprite_height;
     disp_chng = true;
 }
 
 _Bool
 Chip8_OpExec(void){
-    switch((mem[pc] / 16)){
+    switch((mem[pc - 2] / 16)){
         case 0:
             Decode_0();
-            pc+=2;
             break;
         case 1:
             Decode_1();
             break;
         case 6:
             Decode_6();
-            pc+=2;
+            break;
+        case 7:
+            Decode_7();
             break;
         case 0xa:
             Decode_A();
-            pc+=2;
             break;
         case 0xd:
             Decode_D();
-            pc+=2;
             break;
         default:
-            fprintf(stderr, "Unknown OpCode: 0x%x%x\n", (unsigned int)mem[pc], (unsigned int)mem[pc + 1]);
-            pc+=2;
+            fprintf(stderr, "Unknown OpCode: 0x%x%x\n", (unsigned int)mem[pc - 2], (unsigned int)mem[pc - 1]);
             break;
     }
     return true;
